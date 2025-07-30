93da27ec16900df0943efcc38489cca356d478aa
rbose4 <roopbose9@gmail.com>
2025-07-30 19:55:57 -0400

    modified class name in custom_excpetion.py

--- exception/custom_exception.py
+++ exception/custom_exception.py
@@ -3,7 +3,7 @@ import traceback
 from logger.custom_logger import CustomLogger
 
 logger = CustomLogger().get_logger(__file__)
-class CustomException(Exception):
+class DocumentPortalException(Exception):
     """Custom exception class that captures the traceback."""
     def __init__(self, error_message,error_details:sys):
         _,_,exc_tb = error_details.exc_info()
@@ -26,6 +26,6 @@ if __name__ == "__main__":
         a = 1 / 0  # This will raise a ZeroDivisionError
         print(a)
     except Exception as e:
-        app_exc = CustomException(e, sys)
+        app_exc = DocumentPortalException(e, sys)
         logger.error(app_exc)
         raise app_exc
