16548ab61ad34fb9c877aaaecc2d1cfedb5a9c53
rbose4 <roopbose9@gmail.com>
2025-07-30 20:13:51 -0400

    added logging to config loader

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,4 +1,7 @@
 import yaml
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+import sys
 
 def load_config(config_path:str = "config/config.yaml") -> dict:
     """
@@ -10,10 +13,16 @@ def load_config(config_path:str = "config/config.yaml") -> dict:
     Returns:
         dict: Configuration data as a dictionary.
     """
-    with open(config_path, 'r') as file:
-        config = yaml.safe_load(file)
-    return config
-
+    try:
+        with open(config_path, 'r') as file:
+            config = yaml.safe_load(file)
+        return config
+    except Exception as e:
+        app_exc = DocumentPortalException(e, sys)
+        logger = CustomLogger().get_logger(__file__)
+        logger.error(app_exc)
+        raise app_exc
+    
 if __name__ == "__main__":
     # Example usage
     config = load_config()
