a3aa04eaec23f84701adae6aec653c77c465f94b
rbose4 <roopbose9@gmail.com>
2025-07-31 23:08:50 -0400

    updated data_analysis, added attributes to pydantic model

--- model/models.py
+++ model/models.py
@@ -1,2 +1,17 @@
 from pydantic import BaseModel, Field
-from typing import Optional, List, Dict, Any
+from typing import Optional, List, Dict, Any, Union
+
+class MetaData(BaseModel):
+    """
+    Metadata for the document.
+    """
+    Summary:List[str] = Field(default_factory=list, description="Summary of the document")
+    Title:str
+    Author:str
+    DateCreated:str
+    LastModifiedDate:str
+    Publisher:str
+    Language:str
+    PageCount:Union[int,str] # Can be "Not Available" or an integer
+    SentimentTone:str
+    
--- src/doc_analyser/data_analysis.py
+++ src/doc_analyser/data_analysis.py
@@ -10,6 +10,15 @@ from prompt.prompt_library import *
 
 class DocumentAnalyzer:
     def __init__(self):
-        pass
+        self.log = CustomLogger().get_logger(__name__)
+        try:
+            self.model_loader = ModelLoader()
+            self.llm = self.model_loader.load_llm()
+            self.parser = JsonOutputParser()
+            
+        except Exception as e:
+            app_exc = DocumentPortalException(e, sys)
+            self.log.error(app_exc)
+            raise app_exc
     def analyze_document(self):
         pass
