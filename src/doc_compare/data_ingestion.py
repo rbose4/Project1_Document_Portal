98d368128020b8b7590c072948b99ddd8c07ca29
rbose4 <roopbose9@gmail.com>
2025-08-08 14:07:20 -0400

    added code to read pdf file for data ingestion

--- src/doc_compare/data_ingestion.py
+++ src/doc_compare/data_ingestion.py
@@ -12,17 +12,47 @@ class DocumentIngestion:
     for comparison with session-based versioning.
     """
     
-    def __init__(self):
-        pass
+    def __init__(self, base_dir):
+        self.log = CustomLogger().get_logger(__name__)
+        self.base_dir = Path(base_dir)
+        self.base_dir.mkdir(parents=True, exist_ok=True)
     
     def save_uploaded_files(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error saving uploaded files: {e}")
+            raise DocumentPortalException("An error occurred while saving the uploaded files", sys)
     
-    def read__pdf(self):
-        pass
+    def read__pdf(self,pdf_path:Path) -> str:
+        """
+        Read the text content of a PDF file page by page and return the content as a string.
+        """
+        try:
+            with fitz.open(pdf_path) as doc:
+                if doc.is_encrypted:
+                    raise ValueError(f"PDF is encrypted:{pdf_path.name}")
+                all_text = []
+                for page_num in range(doc.page_count):
+                    page = doc.load_page(page_num)
+                    text = page.get_text()
+                    if text.strip():
+                        all_text.append(f"\n --------- Page {page_num+1} ------ \n {text}")
+            self.log.info("PDF read successfully", file=str(pdf_path), pages=len(all_text))
+            return "\n".join(all_text)
+        except Exception as e:
+            self.log.error(f"Error reading PDF: {e}")
+            raise DocumentPortalException("An error occured while reading the PDF", sys)
     
     def combine_pdfs(self):
         pass
     
     def clean_old_sessions(self):
-       pass
+        """
+        Deletes exisiting files at the specified paths
+        """
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error deleting existing files: {e}")
+            raise DocumentPortalException("An error occurred while cleaning old sessions", sys)
