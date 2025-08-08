fe5eb4cb0889ce84c7d34bcd8d66ece3399e40b6
rbose4 <roopbose9@gmail.com>
2025-08-08 19:28:18 -0400

    deleted unwanted space in a multiline prompt

--- prompt/prompt_library.py
+++ prompt/prompt_library.py
@@ -14,18 +14,18 @@ Analyze this document:
 
 # Prompt for document comparison 
 document_comparison_prompt = ChatPromptTemplate.from_template("""
-                                                              You will be provided with two documents.
-                                                              Your tasks are as follows:
-                                                              1. Compare the contents of the two documents.
-                                                              2. Indetify the differences in PDF and note down the page number
-                                                              3. The output you provide must be page wise comparison content
-                                                              4. If any page does not have any changes, you must mention "No Changes" for that page.
-                                                              Input documents:
-                                                              {combined_documents}
-                                                              
-                                                              Your response should follow this format:
-                                                              {format_instructions}
-                                                              """)
+You will be provided with two documents.Your tasks are as follows:
+1. Compare the contents of the two documents.
+2. Indetify the differences in PDF and note down the page number
+3. The output you provide must be page wise comparison content
+4. If any page does not have any changes, you must mention "No Changes" for that page.
+Input documents:
+
+{combined_documents}
+                                    
+Your response should follow this format:
+{format_instructions}
+""")
 
 # central registry for all prompts
 PROMPT_LIBRARY = {
