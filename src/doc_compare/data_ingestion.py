91651644d819879b64c89895303764b16c3be9f1
rbose4 <roopbose9@gmail.com>
2025-08-11 18:31:51 -0400

    code added to save sessions and clean up old sessions

--- src/doc_compare/data_ingestion.py
+++ src/doc_compare/data_ingestion.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import fitz
 import uuid
 from pathlib import Path
@@ -12,10 +13,15 @@ class DocumentIngestion:
     for comparison with session-based versioning.
     """
     
-    def __init__(self, base_dir="data/document_compare"):
+    def __init__(self, base_dir="data/document_compare", session_id = None):
         self.log = CustomLogger().get_logger(__name__)
-        self.base_dir = Path(base_dir)
-        self.base_dir.mkdir(parents=True, exist_ok=True)
+        self.base_dir = Path(base_dir) or Path(os.getenv("DATA_COMPARE_STORAGE_PATH",
+                                                    os.getcwd(),"data","document_compare"))
+        self.session_id = session_id or f"session_{datetime.utcnow().strftime('%Y-%m-%d-H%M%S')}_{uuid.uuid4().hex[:8]}"
+        self.session_path = self.base_dir/ self.session_id
+        self.session_path.mkdir(parents=True, exist_ok=True)
+        
+        self.log.info("Document ingestion initialized for Document Compare module with session path: %s", self.session_path)
     
     def save_uploaded_files(self, reference_file, actual_file):
         try:
@@ -23,9 +29,9 @@ class DocumentIngestion:
             # self.log.info("Existing files deleted successfully.")
             
             # updated version of the file
-            ref_path = self.base_dir/reference_file.name
+            ref_path = self.session_path/reference_file.name
             # original file
-            act_path = self.base_dir/actual_file.name
+            act_path = self.session_path/actual_file.name
             
             if not reference_file.name.endswith('.pdf') or not actual_file.name.endswith('.pdf'):
                 raise ValueError("Only PDF files are supported.")
@@ -67,32 +73,31 @@ class DocumentIngestion:
         Combine contents of reference and actual PDFs into a single string with file name and page numbers.
         """
         try:
-            content_dict = {}
             doc_parts = []
-            for file_name in sorted(self.base_dir.iterdir()):
-                if file_name.is_file() and file_name.suffix==".pdf":
-                    content_dict[file_name.name] = self.read__pdf(file_name)
-            
-            for file_name, content in content_dict.items():
-                doc_parts.append(f"Document:{file_name}\n {content}")
+            for file in sorted(self.session_path.iterdir()):
+                if file.is_file() and file.suffix.lower()==".pdf":
+                    content = self.read__pdf(file)
+                    doc_parts.append(f"Document:{file}\n {content}")
             
             combined_text = "\n\n".join(doc_parts)
-            self.log.info("Documents combined successfully", count=len(doc_parts))
+            self.log.info("Documents combined successfully", count=len(doc_parts), session=self.session_id)
             return combined_text
         except Exception as e:
-            self.log.error(f"Error combining PDFs: {e}")
+            self.log.error(f"Error combining PDFs", error=str(e), session = self.session_id)
             raise DocumentPortalException("An error occurred while combining the PDFs", sys)
                 
-    def clean_old_sessions(self):
+    def clean_old_sessions(self, keep_latest:int=3):
         """
-        Deletes exisiting files at the specified paths
+        Optional method to clean up old sessions folders, keeping only latest N
         """
         try:
             if self.base_dir.exists() and self.base_dir.is_dir():
-                for file in self.base_dir.iterdir():
-                    if file.is_file():
+                session_folders = sorted([f for f in self.base_dir.iterdir() if f.is_dir()],reverse=True)
+                for folder in session_folders[keep_latest:]:
+                    for file in folder.iterdir():
                         file.unlink()
-                        self.log.info("File deleted", path=str(file))
+                    folder.rmdir()
+                    self.log.info("Old sessions cleaned up", count=len(session_folders)-keep_latest, path=str(folder))
                     
         except Exception as e:
             self.log.error(f"Error deleting existing files: {e}")
