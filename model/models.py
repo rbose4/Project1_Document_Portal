691caf263bb54eb50a9263e59fe2607d64026527
rbose4 <roopbose9@gmail.com>
2025-08-13 17:39:53 -0400

    class added for storing prompt types

--- model/models.py
+++ model/models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field, RootModel
 from typing import Optional, List, Dict, Any, Union
+from enum import Enum
 
 class MetaData(BaseModel):
     """
@@ -24,3 +25,9 @@ class ChangeFormat(BaseModel):
 
 class SummaryResponse(RootModel[list[ChangeFormat]]):
     pass
+
+class PromptType(str, Enum):
+    DOCUMENT_ANALYSIS = "document_analysis"
+    DOCUMENT_COMPARISON = "document_comparison"
+    CONTEXTUALIZE_QUESTION = "contextualize_question"
+    CONTEXT_QA = "context_qa"
