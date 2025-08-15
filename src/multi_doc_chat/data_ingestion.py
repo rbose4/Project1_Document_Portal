3bc857f5aa1a342e1439222fe66974e8bf8bae98
rbose4 <roopbose9@gmail.com>
2025-08-15 11:24:04 -0400

    code skeleton added for the multi doc chat

---
+++ src/multi_doc_chat/data_ingestion.py
@@ -0,0 +1,12 @@
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+
+class DataIngestor:
+    def __init__(self):
+        pass
+    
+    def ingest_files(self):
+        pass
+    
+    def _create_retriever(self):
+        pass
