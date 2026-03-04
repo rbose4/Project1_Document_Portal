788ad2097bdc828aad42c031e35fdeca8be76359
rbose4 <roopbose9@gmail.com>
2026-03-04 14:11:57 -0500

    description field added to pydantic models

--- model/models.py
+++ model/models.py
@@ -7,14 +7,14 @@ class MetaData(BaseModel):
     Metadata for the document.
     """
     Summary:List[str] = Field(default_factory=list, description="Summary of the document")
-    Title:str
-    Author:str
-    DateCreated:str
-    LastModifiedDate:str
-    Publisher:str
-    Language:str
-    PageCount:Union[int,str] # Can be "Not Available" or an integer
-    SentimentTone:str
+    Title:str = Field(description="Title of the document")
+    Author:str = Field(description="Author of the document")
+    DateCreated:str = Field(description="The date at which the document was created.")
+    LastModifiedDate:str = Field(description="The date at which the document was last modified.")
+    Publisher:str = Field(description="The publisher of the document")
+    Language:str = Field(description="The language in which the document is written")
+    PageCount:Union[int,str] = Field(description="Total number of pages in the document. It can be an integer or string value Not available") # Can be "Not Available" or an integer
+    SentimentTone:str = Field(description="Sentiment of the document. It can be positive, negative or neutral")
 
 class ChangeFormat(BaseModel):
     """
