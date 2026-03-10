29f492c69a2bb59022e1ff927120f36ee2b9c8f7
rbose4 <roopbose9@gmail.com>
2026-03-10 14:19:08 -0400

    code updated for data ingestion

--- src/multi_doc_chat/data_ingestion.py
+++ src/multi_doc_chat/data_ingestion.py
@@ -2,106 +2,108 @@ import uuid
 import sys
 from pathlib import Path
 from datetime import datetime, timezone
-from langchain_community.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
+from langchain_community.document_loaders import (PyPDFLoader, 
+                                                  Docx2txtLoader,
+                                                  TextLoader)
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 from utils.model_loader import ModelLoader
 
-SUPPORTED_EXTENSIONS = {".pdf", ".docx", ".txt"}
-
 class DocumentIngestor:
-    def __init__(self, temp_dir:str="data/multi_doc_chat", faiss_dir:str="faiss_index", session_id:str=None):
+    SUPPORTED_FILE_TYPES =  {'.pdf','.docx','.txt'}
+    def __init__(self, temp_dir:str = "data/multi_doc_chat",faiss_dir:str="faiss_index", session_id:str|None = None) -> None:
         try:
             self.log = CustomLogger().get_logger(__name__)
             
-            # create base directory for temporary files
+            # create base directories
             self.temp_dir = Path(temp_dir)
             self.faiss_dir = Path(faiss_dir)
-            
-            self.temp_dir.mkdir(parents=True, exist_ok=True)
+            self.temp_dir.mkdir(parents= True, exist_ok=True)
             self.faiss_dir.mkdir(parents=True, exist_ok=True)
             
-            # Create session paths
+            # sessionized directory paths
             self.session_id = session_id or f"session_{datetime.now(timezone.utc).strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4().hex[:8]}"
-            self.session_temp_dir = self.temp_dir / self.session_id
+            self.session_temp_dir = self.temp_dir / self.session_id            
             self.session_faiss_dir = self.faiss_dir / self.session_id
             self.session_temp_dir.mkdir(parents=True, exist_ok=True)
             self.session_faiss_dir.mkdir(parents=True, exist_ok=True)
             
             self.model_loader = ModelLoader()
             
-            self.log.info("Document Ingestor initialized", 
+            self.log.info("DocumentIngestor initialized",
                           temp_base = str(self.temp_dir),
                           faiss_base = str(self.faiss_dir),
                           session_id = self.session_id,
-                          temp_path = str(self.session_temp_dir),
-                          faiss_path = str(self.session_faiss_dir))
+                          temp_path=str(self.session_temp_dir),
+                          faiss_path=str(self.session_faiss_dir))
+            
             
         except Exception as e:
-            self.log.error("Failed to initialize DocumentIngestor", error=str(e))
-            raise DocumentPortalException("Error while initializing DocumentIngestor", sys)
+            self.log.error("Failed to initialize DocumentIngester", error=str(e))
+            raise DocumentPortalException("Intialization error in DocumentIngestor", sys)
     
-    def ingest_files(self, uploaded_files):
+    def ingest_file(self, upload_files):
         try:
             documents = []
-            for upload_file in uploaded_files:
-                extn = Path(upload_file).name.suffix.lower()
-                if extn not in SUPPORTED_EXTENSIONS:
-                    self.log.warning("Unsupported file extension", file_name=upload_file.name, extn=extn)
+            for uploaded_file in upload_files:
+                ext = Path(uploaded_file.name).suffix.lower()
+                if ext not in self.SUPPORTED_FILE_TYPES:
+                    self.log.warning("Unsupported file skipped", filename=uploaded_file.name)
                     continue
-                
-                unique_filename = f"{uuid.uuid4().hex[:8]}{extn}"
+                unique_filename = f"{uuid.uuid4().hex[:8]}{ext}"
                 temp_path = self.session_temp_dir / unique_filename
                 
-                with open(temp_path, "wb") as f:
-                    f.write(upload_file.read())
-                
-                self.log.info("File saved in session folder for ingestion", 
-                              filename = upload_file.name,
-                              saved_path=str(temp_path),
-                              session_id = self.session_id)
-                
-                if extn == ".pdf":
-                    loader = PyPDFLoader(temp_path)
-                elif extn == ".docx":
-                    loader = Docx2txtLoader(temp_path)
-                elif extn == ".txt":
-                    loader = TextLoader(temp_path)
+                with open(temp_path,'wb') as f:
+                    f.write(uploaded_file.read())
+                self.log.info("File saved for ingestion",
+                              filename=uploaded_file.name,
+                              saved_as=temp_path,
+                              session_id=self.session_id)
+                if ext==".pdf":
+                    loader = PyPDFLoader(str(temp_path))
+                elif ext==".txt":
+                    loader = TextLoader(str(temp_path))
+                elif ext == ".docx":
+                    loader = Docx2txtLoader(str(temp_path))
                 else:
-                    self.log.warning("Unsupported file type encountered", filename=upload_file.name)
+                    self.log.warning("Unsupported file type encountered",file_name=uploaded_file.name)
+                    continue
                 
                 docs = loader.load()
                 documents.extend(docs)
-            
+                
             if not documents:
-                raise DocumentPortalException("No valid documents found in uploaded files", sys)
-            
-            self.log.info("All documents loaded", total_docs = len(documents), session_id = self.session_id)
+                raise DocumentPortalException("No valid documents loaded", sys)
+                
+            self.log.info("All documents loaded", total_docs = len(documents), session_id=self.session_id)
+                
             return self._create_retriever(documents)
+                
                 
         except Exception as e:
             self.log.error("Failed to ingest files", error=str(e))
-            raise DocumentPortalException("Error while ingesting files", sys)
+            raise DocumentPortalException("Ingestion error in DocumentIngestor", sys)
     
     def _create_retriever(self, documents):
         try:
-            splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap=300)
+            # create chunks from document objects
+            splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 300)
             chunks = splitter.split_documents(documents=documents)
-            self.log.info("Documents split into chunks", total_chunks=len(chunks), session_id = self.session_id)
+            self.log.info("Documents split into chunks", total_chunks=len(chunks), session_id=self.session_id)
             
+            # Create Faiss vector store
             embedding = self.model_loader.load_embeddings()
             vector_store = FAISS.from_documents(documents=chunks, embedding=embedding)
             
-            # save FAISS index under sesssion folder
+            # Save Faiss index under session folder
             vector_store.save_local(folder_path=str(self.session_faiss_dir))
-            self.log.info("FAISS index saved to disk", path=str(self.session_faiss_dir), session_id=self.session_id)
+            self.log.infor("FAISS index saved to disk", path=str(self.session_faiss_dir), session_id = self.session_id)
             
-            # create retriever
-            retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k":5})
-            self.log.info("FAISS retriever created and ready to use", session_id = self.session_id)
-            return retriever 
+            # Create retriever from vector store
+            retriever = vector_store.as_retriever(search_type="similarity",search_kwargs={"k":5})
+            return retriever
         except Exception as e:
             self.log.error("Failed to create retriever", error=str(e))
-            raise DocumentPortalException("Error while creating retriever", sys)
+            raise DocumentPortalException("Retrieval error in DocumentIngestor", sys)
